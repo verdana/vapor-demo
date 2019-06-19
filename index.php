477c0f052476c18eed60ef3c97d67bff423b367f
Verdana <verdana.cn@gmail.com>
2019-06-19 10:00:38 +0800

    Update 20190619

--- index.php
+++ index.php
@@ -10,16 +10,18 @@ $vapor->addFolder('shared', 'views/shared');
 $vapor->setFileExtension('php');
 // $vapor->loadExtensions('functions.php');
 
-// require_once 'functions.php';
+require_once 'functions.php';
 
-echo $vapor->render('home', [
+$data = [
     'name'   => 'vapor',
     'title'  => 'vapor template engine',
     'author' => 'verdana mu',
     'email'  => 'verdana.cn@gmail.com',
     'github' => 'https://github.com/verdana/vapor.git',
-]);
+];
 
-// $time = microtime(true) - $_SERVER["REQUEST_TIME_FLOAT"];
-// printf("\n<div class=\"time\">%.05f seconds</div>\n", $time);
+// echo $vapor->render('home', $data);
+echo $vapor->make('home')->render($data);
 
+$time = microtime(true) - $_SERVER["REQUEST_TIME_FLOAT"];
+printf("\n<div class=\"time\">%.05f seconds</div>\n", $time);
--- views/home.php
+++ views/home.php
@@ -1,12 +1,13 @@
 <?php $this->layout('layout::main') ?>
 
-<div class="row">
-    <span class="intro">
-        <p><?= $name ?> is a simple PHP template extension that written by C. It's extremely fast, easy to use and easy to extend.</p>
-        <p>It's inspired by the excellent <a href="http://platesphp.com">Plates</a> template engine.</p>
-    </span>
-    <span class="intro">
-        <img src="imgs/p.png" />
-        <p>By now, <?= $name ?> is still under development, doesn't have yet it's complete features.</p>
-    </span>
-</div>
+        <div class="row">
+            <span class="intro">
+                <p><?= $name ?> is a simple PHP template extension that written by C. It's extremely fast, easy to use and easy to extend.</p>
+                <p>It's inspired by the excellent <a href="http://platesphp.com">Plates</a> template engine.</p>
+            </span>
+            <span class="intro">
+                <img src="imgs/p.png" />
+                <p>By now, <?= $name ?> is still under development, doesn't have yet it's complete features.</p>
+            </span>
+        </div>
+
